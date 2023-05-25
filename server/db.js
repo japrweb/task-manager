8dadfd23b05e91479efd2d0e58908360c413daf6
John A. Pérez R <japr893@gmail.com>
2023-05-25 11:05:50 -0400

    Configured server to connect with the database

--- server/db.js
+++ server/db.js
@@ -1,5 +1,9 @@
 import { createPool } from 'mysql2/promise';
 
 export const pool = createPool({
-    
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Deafsound.8',
+    database: 'tasksdb'
 })
