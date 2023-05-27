0bc47a445caee98c7c8036167968ce75ae893da1
John A. Pérez R <japr893@gmail.com>
2023-05-27 17:48:53 -0400

    Server routes

---
+++ server/controllers/tasks.controller.js
@@ -0,0 +1,19 @@
+export const getTasks = (req, res) => {
+    res.send('obteniendo tareas')
+}
+
+export const getTask = (req, res) => {
+    res.send('obteniendo una tarea')
+}
+
+export const createTask = (req, res) => {
+    res.send('creando tarea')
+}
+
+export const updateTask = (req, res) => {
+    res.send('actualizando tarea')
+}
+
+export const delelteTask = (req, res) => {
+    res.send('eliminando tarea')
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
-import express from 'express';
-import { PORT } from './config.js';
-import indexRoutes from './routes/index.routes.js'
+import express from "express";
+import { PORT } from "./config.js";
+
+import indexRoutes from "./routes/index.routes.js";
+import taskRoutes from "./routes/tasks.routes.js";
 
 const app = express();
 
-app.use(indexRoutes)
+app.use(indexRoutes);
+app.use(taskRoutes);
 
-app.listen(PORT)
+app.listen(PORT);
 console.log(`Server is runnig on port ${PORT}`);
