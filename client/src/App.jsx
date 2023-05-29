da4ee8a6bb2802e12b6549871961159efee3f98a
John A. Pérez R <japr893@gmail.com>
2023-05-29 13:39:13 -0400

    Create task frontend - task form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+
 import TasksPage from "./pages/TasksPage";
 import TaskForm from "./pages/TaskForm";
-import NotFound from "./NotFound";
+import NotFound from "./pages/NotFound";
+
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<TasksPage />} />
-      <Route path="/taskform" element={<TaskForm />} />
-      <Route path="*" element={<NotFound />} />
-
-    </Routes>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<TasksPage />} />
+        <Route path="/taskform" element={<TaskForm />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Form, Formik } from "formik";
 
 function TaskForm() {
   return (
-    <div>TaskForm</div>
-  )
+    <div>
+      <Formik
+        initialValues={{
+          title: "",
+          description: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ handleChange, handleSubmit }) => (
+          <Form onSubmit={handleSubmit}>
+            <label>title</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Write a title"
+              onChange={handleChange}
+            />
+
+            <label>description</label>
+            <textarea
+              name="description"
+              rows="3"
+              placeholder="Write a description"
+              onChange={handleChange}
+            ></textarea>
+            <button type="submit">Save</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
-export default TaskForm
+export default TaskForm;
