5e543a36649f9f43cd033bb2df0ea5382a25b9f8
John A. Pérez R <japr893@gmail.com>
2023-05-30 10:23:50 -0400

    List tasks frontend

---
+++ client/src/components/TaskCard.jsx
@@ -0,0 +1,14 @@
+function TaskCard({ task }) {
+  return (
+    <div>
+      <h2>{task.title}</h2>
+      <p>{task.description}</p>
+      <span>{task.done == 1 ? "✔️" : "❌"}</span>
+      <span>{task.createAt}</span>
+      <button>Delete</button>
+      <button>Edit</button>
+    </div>
+  );
+}
+
+export default TaskCard;
--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import { getTasksRequest } from "../api/tasks.api";
+import TaskCard from "../components/TaskCard";
+
 function TasksPage() {
+
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    async function loadTasks() {
+      const response = await getTasksRequest();
+      setTasks(response.data);
+    }
+    loadTasks();
+  }, []);
+
   return (
-    <div>TasksPage</div>
-  )
+    <div>
+      <h1>Tasks</h1>
+
+      {
+        tasks.map(task => (
+          <TaskCard task={task} key={task.id} />
+        ))
+      }
+    </div>
+  );
 }
 
-export default TasksPage
+export default TasksPage;
